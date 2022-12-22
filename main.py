0305321c97ca0f4a07190d14733aec943fc19ac0
Ena <ena.verhorst@arin-innovation.com>
2022-12-22 12:21:21 +0100

    borrar comentarios

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python
-# This is an automatically generated code sample.
-# To make this code sample work in your Oracle Cloud tenancy,
-# please replace the values for any parameters whose current values do not fit
-# your use case (such as resource IDs, strings containing ‘EXAMPLE’ or ‘unique_id’, and
-# boolean, number, and enum parameters with values not fitting your use case).
 import oci
-# Create a default config using DEFAULT profile in default location
-# Refer to
-# https://docs.cloud.oracle.com/en-us/iaas/Content/API/Concepts/sdkconfig.htm#SDK_and_CLI_Configuration_File
-# for more info
+
 config = oci.config.from_file()
 # Initialize service client with default config file
 artifacts_client = oci.artifacts.ArtifactsClient(config)
